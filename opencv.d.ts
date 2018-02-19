139f0684ccf4bc0e8cbced53f59651f5d5982aeb
Martin Guillon <martin.guillon@akylas.fr>
2018-02-19 14:45:36 +0100

    typings

---
+++ opencv.d.ts
@@ -0,0 +1,37 @@
+declare module "opencv" {
+    class Matrix {
+        constructor(height: number, width: number, channels?: number);
+        mean(): number;
+        width(): number;
+        height(): number;
+        channels(): number;
+        toBufferAsync(
+            callback: (err: Error, buffer: Buffer) => void,
+            params?: {
+                ext?: string;
+            }
+        );
+        save(
+            dest: string,
+            callback: (err: Error) => void,
+            params?: {
+                ext?: string;
+            }
+        );
+        toBuffer(params: { ext?: string });
+        put(buffer: Buffer);
+        copy(): Matrix;
+        min(mat1: Matrix, mat2: Matrix): Matrix;
+        max(mat1: Matrix, mat2: Matrix): Matrix;
+        subtract(mat1: Matrix, mat2: Matrix): Matrix;
+        resize(width: number, height: number): Matrix;
+        divide(mat1: Matrix, mat2: Matrix, factor?: number): Matrix;
+        convertTo(mat1: Matrix, type: number, factor?: number): Matrix;
+    }
+    function mean(mats: Matrix[], channels: number): Matrix;
+    function readImageSync(buffer: Buffer): Matrix;
+    function readImage(imgpath: string, callback: (err: Error, mat: Matrix) => void);
+    namespace Constants {
+        const CV_32FC1;
+    }
+}
